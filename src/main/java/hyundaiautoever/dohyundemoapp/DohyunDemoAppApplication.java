151ec65531af31d5bfa706d0e2052ab821aabb93
dohyun93 <kwondohyun93@naver.com>
2023-10-29 16:11:54 +0900

    Initial commit

---
+++ src/main/java/hyundaiautoever/dohyundemoapp/DohyunDemoAppApplication.java
@@ -0,0 +1,13 @@
+package hyundaiautoever.dohyundemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DohyunDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DohyunDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hyundaiautoever/dohyundemoapp/DohyunDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hyundaiautoever.dohyundemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DohyunDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
